c15d42fda3a2683a2ba9d2c016fe3198add0af03
bbailey39 <bbailey39@huskers.unl.edu>
2026-04-04 09:29:26 -0500

    added includes and implemented stylist

--- synchronization/part2/sleepingStylistMon.c
+++ synchronization/part2/sleepingStylistMon.c
@@ -1,27 +1,50 @@
-#define DELAY 1000 
-#define CHAIRS 6
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-void main(void) {
-  // create 75 of customer threads
-  // Create one stylist thread. Don't forget to join threads
+#define CHAIRS 6
+#define DELAY 1000000 // adjust this value
+#define NUM_CUSTOMERS 10
+
+sem_t mutex = 1;
+sem_t stylist_s = 0;
+sem_t customers = 0;
+int waiting = 0;
+
+void main ( void ) {
+// create a specified number of customer threads
+// and a stylist thread . Don ’ t f o r g e t t o j o i n t h r e a d s
 }
 
-void stylist (void) {
-  // add more variables as needed
+void stylist ( void ) {
   int j;
   while (1) {
-    // mon_debugPrint();
-    // mon_checkCustomer();
-    for (j=0; j < DELAY; j++); // Cut hair
+      sem_wait(&customers);
+      sem_wait(&mutex);
+
+      waiting = waiting - 1;
+
+      sem_post(&stylist_s);
+      sem_post(&mutex);
+
+      for (j = 0; j < DELAY; j++);
   }
 }
 
-void customer(void) {
-  // add more variables as needed
+void customer ( void ) {
   int j;
   while (1) {
-    // mon_debugPrint();
-    // if (mon_checkStylist()) break;
-    for (j=0; j<DELAY; j++); // Go shopping
+    down(&mutex);
+    if (waiting < CHAIRS){
+      waiting = waiting + 1;
+      up(&customers);
+      up(&mutex);
+      down(&stylist);
+      break;
+    } else {
+      up(&mutex) ;
+      for (j = 0; j < DELAY; j++); // go shopping
+    }
   }
 }
